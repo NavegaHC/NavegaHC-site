4fd344644d57bb5e1952e70c42d521381580eb56
agathayun <RM561507@fiap.com.br>
2025-05-22 12:27:39 -0300

    arrumando o footer e continuando responsividade

--- js/script-menuhamburguer.js
+++ js/script-menuhamburguer.js
@@ -1,6 +1,5 @@
 const menuHamburguer = document.querySelector('.menu-hamburguer');
-
-const menuOffScreen = document.querySelector('.menu-off-screen')
+const menuOffScreen = document.querySelector('.menu-off-screen');
 
 
 menuHamburguer.addEventListener('click', () => {
